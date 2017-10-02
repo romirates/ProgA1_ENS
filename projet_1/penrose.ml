4efd3c310558aef014c1d274e20555062918010d
Guillaume Barbier <barbier.guillaume60@gmail.com>
2017-10-02 16:31:33 +0200

    Factorized the code and added polymorphism in the penrose project.

--- projet_1/penrose.ml
+++ projet_1/penrose.ml
@@ -1,131 +1,22 @@
-#load "graphics.cma";;
-open Graphics;;
+#use "penrose_triangle.ml"
+#use "tesselation.ml"
 
-(*
- *********************************************************
- *                  Types                                *
- *********************************************************
- *)
+let size = 450.
+and margin = 10.
 
-type point = float * float;;
-type vector = point;;
-type angle = Obtuse | Acute;;
-type triangle = angle * point * point * point;;
-
-(*
- *********************************************************
- *                  Constants                            *
- *********************************************************
- *)
-
-let phi = 1.61803398875;;
-let inv_phi = 0.61803398875;;
-let first_triangle =
-  let len = 500.0 in
-  (Acute,
-  (0. , 0.),
-  (len *. phi , 0.),
-  (1.30901699438 *. len , 0.95105651629 *. len)
-  : triangle)
-;;
-
-(*
- *********************************************************
- *             Data manipulation functions               *
- *********************************************************
- *)
-
-let int_tuple_of_point (x,y :point) =
-  int_of_float x, int_of_float y
-;;
-let get_vector (xa,ya : point) (xb,yb:point) =
-  (xb -. xa, yb -. ya:vector)
-;;
-let add_vector (x,y:point) (u,v:vector) =
-  (x +. u, y +. v : point)
-;;
-let mult_vector scalar (u,v:vector) =
-  (scalar *. u, scalar  *. v : vector)
-;;
-
-(*
- *********************************************************
- *            Printing functions                         *
- *********************************************************
- *)
-
-let init_screen () =
-  close_graph ();
-  open_graph " 800x600-0+0"
-;;
-
-
-let draw (t,a,b,c:triangle) =
-  (*
-   * draws the triangle and its outline
-   *)
-  let xa,ya = int_tuple_of_point a
-  and xb,yb = int_tuple_of_point b
-  and xc,yc = int_tuple_of_point c
-  in let poly = [| xa,ya ; xb,yb ; xc,yc |]
-     in
-     begin match t with
-     |Obtuse -> set_color red
-     |Acute -> set_color green
-     end;
-     fill_poly poly;
-     set_color black;
-     moveto xa ya;
-     lineto xb yb;
-     lineto xc yc;
-     lineto xa ya;
-;;
-
-
-(*
- *********************************************************
- *                     Algorithm                         *
- *********************************************************
- *)
-
-
-let rec divide (t,a,b,c as tri : triangle) generation  =
-  (*
-   * Renders the triangle divided 'generation' times.
-   *)
-  match generation with
-  |0 -> draw tri;
-  |_ ->
-    let new_gen = generation - 1 in
-    begin match t with
-    |Obtuse ->
-      let new_point = add_vector b (mult_vector inv_phi (get_vector b c))
-      in
-      let t1 = (Acute, b, new_point, a : triangle)
-      and t2 = (Obtuse, new_point, c, a : triangle)
-      in
-      
-      divide t1 new_gen;
-      divide t2 new_gen;
-    |Acute ->
-      let new_point1 = add_vector c (mult_vector inv_phi (get_vector c a))
-      and new_point2 = add_vector a (mult_vector inv_phi (get_vector a b))
-      in
-      let t1 = (Acute, c, new_point1, new_point2 : triangle)
-      and t2 = (Acute, c, new_point2, b : triangle)
-      and t3 = (Obtuse, new_point1, a, new_point2 : triangle)
-      in
-      divide t1 new_gen;
-      divide t2 new_gen;
-      divide t3 new_gen;
-    end
-;;
-          
 (*
  *********************************************************
  *                     Tests                             *
  *********************************************************
  *)
 
+let penrose = tesselation draw_triangle_and_outline divide_triangle;;
+let first_triangle = get_basic_acute_triangle size margin;;
+
+let init_screen () =
+  close_graph ();
+  open_graph " 800x600-0+0"
+;;
+
 init_screen ();
-divide first_triangle 6;;
+penrose first_triangle 6;;
---
+++ projet_1/tesselation_extended.ml
@@ -0,0 +1,58 @@
+
+let tesselation_extended speed draw draw_outline draw_separation divide root generations =
+  (*
+   * Divides the object 'root' 'generations' times, and renders each generation.
+   *
+   * - speed : float
+   *   The minimal time in seconds between each generation.
+   *
+   * - draw : 'a -> ()
+   *   Renders an objects.
+   *
+   * - draw_outline : 'a -> ()
+   *   Draws an object's outline.
+   *
+   * - draw_separation : 'a -> ()
+   *   Draws the separation between an object's children.
+   * 
+   * - divide : 'a -> 'a list
+   *   Divides an object into a list of its children.
+   *)
+  
+  (*Remember the triangles already visited in order to draw their children's separation.*)
+  let list_of_visited_nodes = ref []
+  in
+  (*
+   * Draws the current generation, given the list of
+   * objects of this generation.
+   *)
+  let draw_current_gen objects =
+    clear_graph();
+    List.iter draw objects;
+    draw_outline root;
+    List.iter draw_separation !list_of_visited_nodes
+  in
+  (*
+   * 'draw_all_gens' takes the list of objects in one generation,
+   * creates the list of their children through the argument
+   * 'children', then draws the children.
+   * The function does this 'gen' times.
+   *)
+  let rec draw_all_gens generation_list children gen =
+    match gen with
+    | 0 ->()
+    | _ ->
+      begin match generation_list with
+      | node::tail ->
+        let new_children = divide node in
+        list_of_visited_nodes := node::(!list_of_visited_nodes);
+        draw_all_gens tail (new_children @ children) gen
+      | _ ->
+        Unix.sleepf speed;
+        draw_current_gen children;
+        draw_all_gens children [] (gen-1)
+      end
+  in
+  draw_current_gen [root];
+  draw_all_gens [root] [] generations
+;;
